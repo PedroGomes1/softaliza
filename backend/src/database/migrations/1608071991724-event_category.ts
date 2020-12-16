81c623d8f8d8679723aceb6bf9aa509f67ea897e
Pedro <pedro-gomes17@hotmail.com>
2020-12-15 21:01:30 -0300

    Initial application structuring

---
+++ backend/src/database/migrations/1608071991724-event_category.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class eventCategory1608071991724 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
