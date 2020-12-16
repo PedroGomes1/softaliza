24233df22df779c664bbc5acea1c9e9013c6c725
Pedro <pedro-gomes17@hotmail.com>
2020-12-16 11:43:55 -0300

    Index route

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,20 @@
 import Router from 'express';
 
+import Event from './event.routes';
+import CategoryEvent from './category-event.routes';
+import Session from './session.routes';
+import User from './user.routes';
+
+import EnsureAuthenticate from '../middleware/EnsureAuthenticate';
+
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json('');
-});
+routes.use('/session', Session);
+routes.use('/user', User);
+
+routes.use(EnsureAuthenticate);
+
+routes.use('/event', Event);
+routes.use('/event-category', CategoryEvent);
+
 export default routes;
