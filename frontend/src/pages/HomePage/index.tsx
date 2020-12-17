98ec335ffe3b951b7b5634ef837c09f176b5c362
Pedro <pedro-gomes17@hotmail.com>
2020-12-17 14:27:47 -0300

    Home page and its styling

--- frontend/src/pages/HomePage/index.tsx
+++ frontend/src/pages/HomePage/index.tsx
@@ -1,9 +1,56 @@
-import React from "react";
-
-import { Container } from "./styles";
+import React, { useCallback } from "react";
+import { useHistory } from "react-router-dom";
+import eventsImage from "../../assets/images/event-home.svg";
+import logoEventSystem from "../../assets/images/logo-event-system.png";
+import { Container, Header, Navigation, Separator, Content } from "./styles";
 
 const HomePage: React.FC = () => {
-  return <Container />;
+  const history = useHistory();
+
+  const handleNavigateToListEvents = useCallback(() => {
+    history.push("/events");
+  }, [history]);
+
+  const handleNavigateToSignIn = useCallback(() => {
+    history.push("/signin");
+  }, [history]);
+
+  const handleNavigateToSignUp = useCallback(() => {
+    history.push("/signup");
+  }, [history]);
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoEventSystem} alt="" />
+
+        <Navigation>
+          <button type="button" onClick={handleNavigateToListEvents}>
+            Eventos
+          </button>
+          <Separator />
+          <button type="button" onClick={handleNavigateToSignIn}>
+            Login
+          </button>
+          <button type="button" onClick={handleNavigateToSignUp}>
+            Criar conta
+          </button>
+        </Navigation>
+      </Header>
+
+      <Content>
+        <div>
+          <h1>A plataforma de organizações de eventos presenciais e online.</h1>
+          <h3>
+            Confira o mais moderno ambiente capaz de suprir suas necessidades
+            organizacionais de eventos
+          </h3>
+        </div>
+
+        <img src={eventsImage} alt="" />
+      </Content>
+    </Container>
+  );
 };
 
 export default HomePage;
--- frontend/src/pages/HomePage/styles.ts
+++ frontend/src/pages/HomePage/styles.ts
@@ -1,3 +1,91 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  max-width: 192rem;
+  min-height: 100vh;
+`;
+export const Header = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 2.5rem 3rem;
+
+  img {
+    width: 15rem;
+    height: 5rem;
+  }
+`;
+export const Navigation = styled.div`
+  display: flex;
+  align-items: center;
+
+  button {
+    width: 15rem;
+    height: 5rem;
+
+    background: transparent;
+
+    border: 0.3rem solid #3282b8;
+    color: #ffffff;
+    border-radius: 3rem;
+    margin-left: 2rem;
+
+    font-size: 1.6rem;
+    font-family: "Poppins", sans-serif;
+
+    transition: 0.5s;
+  }
+
+  button:hover {
+    background-color: #3282b8;
+  }
+`;
+
+export const Separator = styled.div`
+  width: 0.1rem;
+  height: 3rem;
+  background: #ffffff;
+
+  margin-left: 2rem;
+`;
+export const Content = styled.main`
+  display: flex;
+  align-items: center;
+
+  padding: 3rem;
+  max-height: 100%;
+
+  > div {
+    flex-direction: column;
+
+    max-width: 40%;
+
+    h1,
+    h3 {
+      color: #ffffff;
+      letter-spacing: 0.1rem;
+    }
+
+    h1 {
+      font-size: 4.6rem;
+    }
+
+    h3 {
+      margin-top: 3rem;
+      color: #b5b5b5;
+      font-size: 1.4rem;
+      line-height: 2.5rem;
+      letter-spacing: 0.15rem;
+    }
+  }
+
+  img {
+    width: 65rem;
+    height: 49rem;
+
+    flex: 1;
+  }
+`;
