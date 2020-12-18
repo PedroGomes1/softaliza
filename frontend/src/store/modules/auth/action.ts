1fdc73a2e29eda445cf496f4935c276c0cd62207
Pedro <pedro-gomes17@hotmail.com>
2020-12-18 01:22:22 -0300

    Actions auth redux

---
+++ frontend/src/store/modules/auth/action.ts
@@ -0,0 +1,26 @@
+import { SignInProps, UserLoggedProps, ActionTypes } from "./types";
+
+export function signInRequest({ email, password }: SignInProps) {
+  return {
+    type: ActionTypes.signInRequest,
+    payload: {
+      email,
+      password,
+    },
+  };
+}
+
+export function signInSuccess(user: UserLoggedProps) {
+  return {
+    type: ActionTypes.signInSuccess,
+    payload: {
+      user,
+    },
+  };
+}
+
+export function signInFailure() {
+  return {
+    type: ActionTypes.signInFailure,
+  };
+}
