f9d9b5b29ace073e57a3c37f7f3b096073be5ab1
Pedro <pedro-gomes17@hotmail.com>
2020-12-18 01:28:50 -0300

    Creation of the main page including your route in the route file

---
+++ frontend/src/pages/MainPage/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+const MainPage: React.FC = () => {
+  return <h1>Main page</h1>;
+};
+
+export default MainPage;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,6 +5,7 @@ import HomePage from "../pages/HomePage";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
 import Events from "../pages/Events";
+import MainPage from "../pages/MainPage";
 
 import Route from "./Route";
 
@@ -15,6 +16,7 @@ const Routes: React.FC = () => (
       <Route path="/signin" exact component={SignIn} />
       <Route path="/signup" exact component={SignUp} />
       <Route path="/events" exact component={Events} />
+      <Route path="/mainpage" exact component={MainPage} isPrivate />
     </Switch>
   </BrowserRouter>
 );
