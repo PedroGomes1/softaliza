949042e700fafd4080eeb5c3940e670923fe38c9
Pedro <pedro-gomes17@hotmail.com>
2020-12-19 01:26:03 -0300

    Validation event fields

---
+++ frontend/src/validations/Event.ts
@@ -0,0 +1,17 @@
+import * as yup from "yup";
+
+const eventSchema = yup.object().shape({
+  title: yup.string().required("Campo obrigatório!"),
+  description: yup.string().required("Campo obrigatório!"),
+  date: yup.string().required("Campo obrigatório!"),
+  hour: yup.string().required("Campo obrigatório!"),
+  category_id: yup.string().required("Campo obrigatório!"),
+  email: yup
+    .string()
+    .email("Digite seu e-mail no formato correto")
+    .required("Campo obrigatório!"),
+  telephone: yup.string().required("Campo obrigatório!"),
+  address: yup.string().required("Campo obrigatório!"),
+});
+
+export default eventSchema;
