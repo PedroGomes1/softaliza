cd5769d0190a827aff50d80a690dc959fe4af06d
Pedro <pedro-gomes17@hotmail.com>
2020-12-19 01:25:42 -0300

    Main page and its styling

--- frontend/src/pages/MainPage/index.tsx
+++ frontend/src/pages/MainPage/index.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { MdSearch, MdEdit, MdDeleteForever } from "react-icons/md";
+import Header from "../../components/Header";
+import CardEvent from "../../components/CardsEvent";
 
-// import { Container } from './styles';
+import { Container, Wrapper, WrapperActionsCard } from "./styles";
+import Input from "../../components/Input";
 
 const MainPage: React.FC = () => {
-  return <h1>Main page</h1>;
+  const [titleSearch, setTitleSearch] = useState("");
+
+  const { register } = useForm();
+
+  return (
+    <Container>
+      <Header />
+
+      <Wrapper>
+        <h1>Eventos cadastrados</h1>
+
+        <Input
+          icon={MdSearch}
+          placeholder="Buscar pelo titulo do evento"
+          name="search"
+          type="text"
+          register={register}
+          onChange={(event) => setTitleSearch(event.target.value)}
+        />
+      </Wrapper>
+
+      <CardEvent titleSearch={titleSearch}>
+        <WrapperActionsCard>
+          <MdEdit size={25} color="#000000" />
+          <MdDeleteForever size={25} color="#f64c75" />
+        </WrapperActionsCard>
+      </CardEvent>
+    </Container>
+  );
 };
 
 export default MainPage;
--- frontend/src/pages/MainPage/styles.ts
+++ frontend/src/pages/MainPage/styles.ts
@@ -1,3 +1,32 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  h1 {
+    color: #ffffff;
+    text-align: center;
+    font-size: 4rem;
+  }
+`;
+
+export const Wrapper = styled.div`
+  width: 50%;
+  margin: 0 auto;
+
+  @media (max-width: 530px) {
+    width: 85%;
+  }
+`;
+
+export const WrapperActionsCard = styled.div`
+  display: flex;
+  justify-content: center;
+
+  svg {
+    margin: 1rem 0.5rem;
+    transition: 0.2s;
+    :hover {
+      background: #dedee4;
+      padding: 0.1rem;
+    }
+  }
+`;
